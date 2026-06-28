0d38e3c421b434ba183807c77e56c172caf037d2
Tamjid <Tamjid.Islam@city.ac.uk>
2026-06-28 21:45:20 +0100

    added a loadSong function that loads the new song file and updates the title, variable duration stores the initial time for the song to start and updated via audio buit in function and an event listener. Fixed some bugs by rearranging the order of some lines of code which contributed greatly to the function of some code

--- script.js
+++ script.js
@@ -5,43 +5,75 @@ const playBtn =  document.querySelector(".playBtn");
 const prevBtn = document.querySelector(".prevBtn");
 const skipBtn = document.querySelector(".skipBtn");
 
+//audio
+const audio = new Audio();
+
 
 let songs = [
-    {title: "Her Old Friends",artist: "PartynextDoor"},
-    {title: "Lights Out", artist: "Chris Brown"},
-    {title: "Brand New", artist: "Drake"},
-    {title: "Come Through", artist: "H.E.R"},
-    {title: "Slow Down", artist: "Chase Atlantic"}
+    {title: "Her Old Friends",artist: "PartynextDoor", file: "assets/music/herOldFriends.mp3"},
+    {title: "Lights Out", artist: "Chris Brown", file: "assets/music/herOldFriends.mp3"},
+    {title: "Brand New", artist: "Drake", file: "assets/music/herOldFriends.mp3"},
+    {title: "Come Through", artist: "H.E.R", file: "assets/music/herOldFriends.mp3"},
+    {title: "Slow Down", artist: "Chase Atlantic", file: "assets/music/herOldFriends.mp3"}
 ]
 
 let isPlaying = false;
 let currentSongIndex = 0;
+let duration = 0;
 
+audio.addEventListener("loadedmetadata", () => {
+    duration = audio.duration;
+    console.log(duration)
+});
+
+function loadSong(){
+    songTitle.textContent = songs[currentSongIndex].title;
+    audio.src = songs[currentSongIndex].file;
+}
+loadSong();
 
 playBtn.addEventListener("click", () => {
     if (isPlaying === false){
-        playBtn.textContent = "⏸";
-        // songTitle.textContent = "Playing";
         isPlaying = true;
+        playBtn.textContent = "⏸";
+        audio.play();
     }else{
         playBtn.textContent = "▶︎";
-        // songTitle.textContent = "Paused";
         isPlaying = false;
+        audio.pause();
     }
 });
 
 skipBtn.addEventListener("click", () => {
     currentSongIndex += 1;
+
     if (currentSongIndex >= songs.length){
         currentSongIndex = 0;
     }
-    songTitle.textContent = songs[currentSongIndex].title;
+
+    audio.pause();
+    isPlaying = false;
+    playBtn.textContent = "▶︎";
+    loadSong();
+
+    if (isPlaying){
+        audio.play();
+    }
 });
 
 prevBtn.addEventListener("click", () => {
     currentSongIndex -= 1
+
     if (currentSongIndex < 0){
         currentSongIndex = songs.length-1;
     }
-    songTitle.textContent = songs[currentSongIndex].title;
+
+    audio.pause();
+    isPlaying = false;
+    playBtn.textContent = "▶︎";
+    loadSong();
+
+    if (isPlaying){
+        audio.play();
+    }
 });
